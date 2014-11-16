d22d87384b7a07fee1510ac27bae2a96e7be0c12
Poren Chiang <ren.chiang@gmail.com>
2014-11-17 02:28:43 +0800

    Update models

--- core/models.py
+++ core/models.py
@@ -9,3 +9,6 @@ class AuthCode(models.Model):
     code = models.CharField(max_length=256)
     issued = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.code
