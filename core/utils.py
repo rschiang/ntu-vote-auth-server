046dbbabbb7208f95f1f296c0577301e2b8ccdda
Poren Chiang <ren.chiang@gmail.com>
2014-12-16 14:44:00 +0800

    Normalize coop member data

--- core/utils.py
+++ core/utils.py
@@ -1,3 +1,4 @@
+import re
 from .models import AuthCode, Record, CooperativeMember
 
 def import_auth_code(filename=None):
@@ -25,7 +26,10 @@ def import_coop_member(filename=None):
         members = {}
         for line in f:
             serial, _, student_id = line.partition(',')
-            if not serial or not student_id:
+            serial = serial.strip()
+            student_id = student_id.strip()
+
+            if not (serial and student_id and re.match(r'[A-Z]\d{2}[0-9AB]\d{6}', student_id)):
                 print('Invalid line:', line)
                 continue
             elif student_id in members:
@@ -33,8 +37,8 @@ def import_coop_member(filename=None):
                 print('Duplicate entry:', member.serial, member.student_id)
 
             member = CooperativeMember()
-            member.serial = serial.strip()
-            member.student_id = student_id.strip()
+            member.serial = serial
+            member.student_id = student_id
             members[student_id] = member
 
         CooperativeMember.objects.bulk_create(members.values())
