d5bbc2dbc3496ad8e5305dec1e9428b221c176f0
Poren Chiang <ren.chiang@gmail.com>
2014-12-16 15:00:49 +0800

    Implement wipe functions

--- core/utils.py
+++ core/utils.py
@@ -27,9 +27,9 @@ def import_coop_member(filename=None):
         for line in f:
             serial, _, student_id = line.partition(',')
             serial = serial.strip()
-            student_id = student_id.strip()
+            student_id = student_id.strip().upper()
 
-            if not (serial and student_id and re.match(r'[A-Z]\d{2}[0-9AB]\d{6}', student_id)):
+            if not (serial and re.match(r'[A-Z]\d{2}[0-9AB]\d{5}', student_id)):
                 print('Invalid line:', line)
                 continue
             elif student_id in members:
@@ -42,6 +42,24 @@ def import_coop_member(filename=None):
             members[student_id] = member
 
         CooperativeMember.objects.bulk_create(members.values())
+
+def reset_server_state():
+    auth_codes = AuthCode.objects.filter(issued=True)
+    records = Record.objects.all()
+    print('Issued auth codes:', auth_codes.count())
+    print('Record entries:', records.count())
+
+    row_count = auth_codes.update(issued=False)
+    print('... reset %s rows.', row_count)
+
+    records.delete()
+    print('... cleared.')
+
+def wipe_auth_code():
+    AuthCode.objects.all().delete()
+
+def wipe_coop_member():
+    CooperativeMember.objects.all().delete()
 
 def get_student(student_id):
     return Record.objects.get(student_id=student_id)
