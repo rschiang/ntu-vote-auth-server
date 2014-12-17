db351ec2a534c7eefcfc164ba65de5c968bc8c4d
Poren Chiang <ren.chiang@gmail.com>
2014-12-17 23:45:23 +0800

    Refactor logging configuration in settings

--- core/settings.py
+++ core/settings.py
@@ -89,66 +89,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Logging
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'file'],
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.security': {
-            'handlers': ['mail_admins', 'file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'py.warnings': {
-            'handlers': ['console', 'file'],
-        },
-        'vote': {
-            'handlers': ['mail_admins', 'file'],
-            'level': 'DEBUG',
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'formatters': {
-        'default': {
-            'format': '%(levelname)s %(asctime)s\n%(message)s\n'
-        },
-    },
-    'handlers': {
-        'null': {
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'INFO',
-            # 'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'vote.log'),
-            'formatter': 'default',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-}
+from .log import LOGGING_DIR, LOGGING
 
 # App configurations
 API_KEY = os.environ.get('VOTE_API_KEY')
