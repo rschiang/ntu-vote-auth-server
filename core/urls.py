7e3ec5eeb8ce9c28e8a62ab77153970a8bc9c51c
Squirrel <kanade123321@gmail.com>
2016-04-22 19:45:46 +0800

    update url config

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from .views import (index, authenticate, confirm, report, complete)
+from account.views import (register, ping)
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'core.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'core.views.index'),
-    url(r'^api/station/register$', 'account.views.register'),
-    url(r'^api/station/ping$', 'account.views.ping'),
-    url(r'^api/authenticate$', 'core.views.authenticate'),
-    url(r'^api/confirm$', 'core.views.confirm'),
-    url(r'^api/report$', 'core.views.report'),
-    url(r'^api/complete$', 'core.views.complete', name='callback'),
-)
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^api/station/register$', register),
+    url(r'^api/station/ping$', ping),
+    url(r'^api/authenticate$', authenticate),
+    url(r'^api/confirm$', confirm),
+    url(r'^api/report$', report),
+    url(r'^api/complete$', complete, name='callback'),
+]
