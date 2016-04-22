da10b596e8fb92899e0da5c8066499d3deeaca95
Squirrel <kanade123321@gmail.com>
2016-04-22 23:39:10 +0800

    use new version url format

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,10 @@ from account.views import (register, ping)
 
 urlpatterns = [
     url(r'^$', index),
-    url(r'^api/station/register$', register),
-    url(r'^api/station/ping$', ping),
-    url(r'^api/authenticate$', authenticate),
-    url(r'^api/confirm$', confirm),
-    url(r'^api/report$', report),
+    url(r'^api/station/register$', register, name='register'),
+    url(r'^api/station/ping$', ping, name='ping'),
+    url(r'^api/authenticate$', authenticate, name='authenticate'),
+    url(r'^api/confirm$', confirm, name='confirm'),
+    url(r'^api/report$', report, name='report'),
     url(r'^api/complete$', complete, name='callback'),
 ]
