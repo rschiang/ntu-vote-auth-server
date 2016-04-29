deb8cb8c0b726e74db78a331bec5b7d9d2533cc4
Squirrel <kanade123321@gmail.com>
2016-04-29 21:30:04 +0800

    simplized API ping

--- account/views/ping.py
+++ account/views/ping.py
@@ -1,26 +1,11 @@
-from account.models import Session
 from django.utils import timezone
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from core.views.decorators import check_prerequisites
-from core.views.utils import error, logger
+from core.views.decorators import login_required
 
 
 @api_view(['POST'])
-@check_prerequisites('token')
+@login_required
 def ping(request):
-    token = request.data['token']
     current_time = timezone.now()
-    try:
-        session = Session.objects.get(token=token)
-        if current_time >= session.expired_on:
-            logger.error('Session expired for token %s', token)
-            return error('unauthorized', status.HTTP_401_UNAUTHORIZED)
-    except Session.DoesNotExist:
-        logger.error('Ping attempted failed for token %s', token)
-        return error('unauthorized', status.HTTP_401_UNAUTHORIZED)
-
-    session.last_seen = current_time
-    session.save()
     return Response({'status': 'success', 'timestamp': current_time.isoformat()})
