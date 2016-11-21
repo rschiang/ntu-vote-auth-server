c89d2c68d9134064cf5d1dbaf7437e783ba94b51
Squirrel <azdkj532@ntuosc.org>
2016-11-21 12:03:33 +0800

    update url list

--- staff/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from staff.views import (
-    reset_list,
-)
-from account.views import (register, ping)
-
-urlpatterns = [
-    url(r'^reset/$', reset_list, name='reset_list'),
-]
