2eea87b8b78644db95e38b8e0f9b798c2f8fad1c
Squirrel <azdkj532@ntuosc.org>
2017-04-13 10:36:49 +0800

    add implementaion of Entry rules

---
+++ core/entry.py
@@ -0,0 +1,33 @@
+from core.service import BaseEntryRule
+from core.service import entry_provider
+from core.models import Entry, OverrideEntry
+
+
+class NormalEntryRule(BaseEntryRule):
+    queryset = Entry.objects.all()
+    lookup_field = 'dpt_code'
+    entry_field = 'kind'
+
+    def get_object(self, student_info):
+        try:
+            entry = self.get_queryset().get(dpt_code=student_info.department)
+            return entry.kind
+        except Entry.DoesNotExist:
+            return None
+
+
+class OverrideEntryRule(BaseEntryRule):
+    queryset = OverrideEntry.objects.all()
+    lookup_field = 'student_id'
+    entry_field = 'entry.kind'
+
+    def get_object(self, student_info):
+        try:
+            entry = self.get_queryset().get(student_id=student_info.id)
+            return entry.kind
+        except OverrideEntry.DoesNotExist:
+            return None
+
+
+entry_provider.register('normal', NormalEntryRule)
+entry_provider.register('override', OverrideEntryRule)
