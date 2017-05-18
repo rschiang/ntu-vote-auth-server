3a6dc521945e42bb70c1b3f607ccfcd530e713f9
Poren Chiang <ren.chiang@gmail.com>
2017-05-18 23:34:08 +0800

    Initial implementation of stat gen script

---
+++ stat.py
@@ -0,0 +1,100 @@
+from account.models import Station
+from core.models import AuthToken
+from django.conf import settings
+from django.utils.timezone import localtime
+
+# Pre-generate constants
+STATIONS = { station.external_id: station.name for station in Station.objects.all() }  # noqa: E305
+START_TIME_INDEX = calculate_time_index(settings.EVENT_START_DATE) - 1
+END_TIME_INDEX = calculate_time_index(settings.EVENT_END_DATE) + 1
+
+# Helper classes
+class Table(object):
+    def __init__(self, rows=None, cols=None, default=0):
+        self.data = { row: { col: default for col in cols } for row in rows }
+        self.rows = list(rows)
+        self.cols = list(cols)
+
+    def get(self, x, y):
+        return self.data[x][y]
+
+    def set(self, x, y, value):
+        self.data[x][y] = value
+
+    def increase(self, x, y):
+        self.data[x][y] += 1
+
+    def print_out(self, x_transform=None, y_transform=None, print_sum=False):
+        x_transform = x_transform or (lambda x: x)
+        y_transform = y_transform or (lambda y: y)
+        if print_sum:
+            x_sum, y_sum = self.sum()
+            self.rows = sorted(self.rows, key=lambda i: x_sum[i], reverse=True)
+
+        print('[', end='')
+        print(*(y_transform(y) for y in self.cols), ']', sep=', ')
+        print('{')
+        for x in self.rows:
+            print('  ', x_transform(x), ': [', end='')
+            print(*(self.data[x][y] for y in self.cols), sep=', ', end='')
+            print('],')
+        if print_sum:
+            print('sum: [', end='')
+            print(*(y_sum[y] for y in self.cols), sep=', ', end='')
+            print('],')
+        print('}')
+
+    def sum(self):
+        x_sum = { row: 0 for row in self.rows }
+        y_sum = { col: 0 for col in self.cols }
+        for x in self.rows:
+            for y in self.cols:
+                val = self.data[x][y]
+                x_sum[x] += val
+                y_sum[y] += val
+        return x_sum, y_sum
+
+    def aggregate(self):
+        for x in self.rows:
+            count = 0:
+            for y in self.cols:
+                count += self.data[x][y]
+                self.data[x][y] = count
+
+    @classmethod
+    def generate(cls, items, row_attr, rows, col_attr, cols, default=0):
+        table = cls(rows=rows, cols=cols, default=default)
+        for item in items:
+            table.increase(item.__dict__[row_attr], item.__dict__[col_attr])
+        return table
+
+class Item(object):
+    def __init__(self, token=None):
+        self.standing = token.student_id[:3]
+        self.college = token.kind[0]
+        self.station_id = token.station_id
+        self.time_index = calculate_time_index(token.timestamp) - START_TIME_INDEX
+
+# Utility functions
+def calculate_time_index(t):
+    t = localtime(t)
+    return t.hour * 2 + (1 if t.minute >= 30 else 0)
+
+def time_index_to_str(i):
+    digit = ((i - 24) if i >= 26 else i) / 2
+    half = ':30' if i % 2 == 1 else ''
+    noon = 'pm' if i >= 24 else 'am'
+    return '{}{}{}'.format(digit, half, noon)
+
+# Table generation
+def print_station_time_table():
+    table = Table.generate(items, 'station_id', STATIONS.keys(), 'time_index', range(START_TIME_INDEX, END_TIME_INDEX + 1), 0)
+    table.print_out(lambda x: STATIONS[x], time_index_to_str, print_sum=True)
+    return table
+
+
+if __name__ == '__main__':
+    items = [Item(token) for token in AuthToken.objects.filter(issued=True)]
+    station_time_table = print_station_time_table()
+    station_time_table.aggregate()
+    station_time_table.print_out(lambda x: STATIONS[x], time_index_to_str, print_sum=False)
