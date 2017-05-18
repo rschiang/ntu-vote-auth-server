5fe2b23e0ce3ba4000ce4a98a4832565e653e9b3
Poren Chiang <ren.chiang@gmail.com>
2017-05-18 23:57:43 +0800

    Add station-college stat gen functions

--- stat.py
+++ stat.py
@@ -7,6 +7,19 @@ from django.utils.timezone import localtime
 STATIONS = { station.external_id: station.name for station in Station.objects.all() }  # noqa: E305
 START_TIME_INDEX = calculate_time_index(settings.EVENT_START_DATE) - 1
 END_TIME_INDEX = calculate_time_index(settings.EVENT_END_DATE) + 1
+COLLEGES = {
+    '1': '文學院',
+    '2': '理學院',
+    '3': '社會科學院',
+    '4': '醫學院',
+    '5': '工學院',
+    '6': '生物資源暨農學院',
+    '7': '管理學院',
+    '8': '公共衛生學院',
+    '9': '電機資訊學院',
+    'A': '法律學院',
+    'B': '生命科學院',
+}
 
 # Helper classes
 class Table(object):
@@ -61,6 +74,10 @@ class Table(object):
                 count += self.data[x][y]
                 self.data[x][y] = count
 
+    def transpose(self):
+        self.data = { y: { x: self.data[x][y] for x in self.rows } for y in self.cols }
+        self.rows, self.cols = self.cols, self.rows
+
     @classmethod
     def generate(cls, items, row_attr, rows, col_attr, cols, default=0):
         table = cls(rows=rows, cols=cols, default=default)
@@ -88,13 +105,27 @@ def time_index_to_str(i):
 
 # Table generation
 def print_station_time_table():
-    table = Table.generate(items, 'station_id', STATIONS.keys(), 'time_index', range(START_TIME_INDEX, END_TIME_INDEX + 1), 0)
+    table = Table.generate(items, 'station_id', STATIONS.keys(), 'time_index', range(START_TIME_INDEX, END_TIME_INDEX + 1))
     table.print_out(lambda x: STATIONS[x], time_index_to_str, print_sum=True)
+    return table
+
+def print_station_college_table():
+    table = Table.generate(items, 'station_id', STATIONS.keys(), 'college', COLLEGES.keys())
+    table.print_out(lambda x: STATIONS[x], lambda y: COLLEGES[y], print_sum=True)
     return table
 
 
 if __name__ == '__main__':
     items = [Item(token) for token in AuthToken.objects.filter(issued=True)]
+
+    print('\n<Station-Time>')
     station_time_table = print_station_time_table()
     station_time_table.aggregate()
+    print('\n<Station-Time-Aggr>')
     station_time_table.print_out(lambda x: STATIONS[x], time_index_to_str, print_sum=False)
+
+    print('\n<Station-College>')
+    station_college_table = print_station_college_table()
+    station_college_table.transpose()
+    print('\n<College-Station>')
+    station_college_table.print_out(lambda x: STATION[x], lambda y: COLLEGES[y], print_sum=True)
