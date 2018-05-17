62f24b624f35c1a458d4042aed0626ac90d85d10
Poren Chiang <ren.chiang@gmail.com>
2018-05-17 20:22:09 +0800

    Remove app-specific config

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,10 @@
 # Models
-from .ballot import Ballot      # noqa: F401
-from .election import Election  # noqa: F401
+# flake8: noqa: F401
+from .auth_code import AuthCode
+from .ballot import Ballot
+from .election import Election
+from .elector import Elector
+from .errors import InvalidState
+from .session import Session
+from .station import Station
+from .validators import validate_student_id
