ae666921b566efd4ca3afee61a0228b1e7e7d957
Poren Chiang <ren.chiang@gmail.com>
2018-05-31 05:39:58 +0800

    Note to reset Election state

--- core/management/commands/resetvote.py
+++ core/management/commands/resetvote.py
@@ -1,5 +1,5 @@
 import os
-from core.models import Session
+from core.models import Session, Election
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -7,7 +7,12 @@ class Command(BaseCommand):
     help = 'Reset vote server'
 
     def handle(self, *args, **options):
+        election = Election.objects.get()
+        election.state = Election.NOT_STARTED
+        election.start_time = None
+        election.save()
+
         Session.objects.exclude(state=Session.BANNED).delete()
         logging_dir = settings.LOGGING_DIR
         for f in os.listdir(logging_dir):
-            os.truncate(os.path.join(logging_dir, f))
+            os.truncate(os.path.join(logging_dir, f), 0)
